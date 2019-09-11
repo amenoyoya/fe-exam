150f24ca705a39bc4c378da57bf29d0ea187e376
amenoyoya <mtyswe3@gmail.com>
2019-09-11 19:32:30 +0900

    make: flaskサーバー化

---
+++ app/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav-bar></nav-bar>
+    <section class="section">
+      <router-view></router-view>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    if (this.$route.query.redirect) {
+      // redirect先がクエリに指定されているならリダイレクト
+      this.$router.push(this.$route.query.redirect);
+    } else {
+      // this.$router.push('/dashboard/');
+    }
+  }
+}
+</script>
