57021dea595d7ce45cd10ed1ebb53f56414060bd
amenoyoya <mtyswe3@gmail.com>
2019-10-19 17:48:45 +0900

    make: 基本情報技術者試験Ankiパッケージ

--- convert_quiz.py
+++ convert_quiz.py
@@ -1,41 +1,41 @@
 import genanki, random
 from markdown import markdown
 
-# convert markdown code to html: str -> str
-def convert_markdown(md: str) -> str:
-    return markdown(md, extensions=[
-        'extra', 'admonition', 'codehilite', 'meta', 'nl2br', 'sane_lists', 'smarty', 'wikilinks'
-    ])
-
-# convert markdown file to html: str -> str
-def load_markdown(filename: str) -> str:
-    with open(filename, 'rb') as f:
-        return convert_markdown(f.read().decode('utf-8'))
-
-# Anki Deck
-deck = genanki.Deck(
-  random.randrange(1 << 30, 1 << 31), # 任意のデッキIDを振る
-  '基本情報技術者試験' # デッキ名
-)
-
-# Anki Model: Card Stylings
-model = genanki.Model(
-  random.randrange(1 << 30, 1 << 31), # 任意のモデルIDを振る
-  '基本情報技術者試験', # モデル名
-  fields=[
-    {'name': 'Question'},
-    {'name': 'Answer'},
-  ],
-  templates=[
-    {
-      'name': 'Card 1',
-      'qfmt': '{{Question}}',
-      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
-    },
-  ]
-)
-
 def create_anki_package(name: str, num: int) -> None:
+    # convert markdown code to html: str -> str
+    def convert_markdown(md: str) -> str:
+        return markdown(md, extensions=[
+            'extra', 'admonition', 'codehilite', 'meta', 'nl2br', 'sane_lists', 'smarty', 'wikilinks'
+        ])
+
+    # convert markdown file to html: str -> str
+    def load_markdown(filename: str) -> str:
+        with open(filename, 'rb') as f:
+            return convert_markdown(f.read().decode('utf-8'))
+
+    # Anki Deck
+    deck = genanki.Deck(
+        random.randrange(1 << 30, 1 << 31), # 任意のデッキIDを振る
+        '基本情報技術者試験' # デッキ名
+    )
+
+    # Anki Model: Card Stylings
+    model = genanki.Model(
+        random.randrange(1 << 30, 1 << 31), # 任意のモデルIDを振る
+        '基本情報技術者試験', # モデル名
+        fields=[
+            {'name': 'Question'},
+            {'name': 'Answer'},
+        ],
+        templates=[
+            {
+            'name': 'Card 1',
+            'qfmt': '{{Question}}',
+            'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+            },
+        ]
+    )
+
     # add Anki cards
     for i in range(1, num + 1):
         dir = './{:s}/{:03d}'.format(name, i)
